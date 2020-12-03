cb981d1316b8c4d12189470c36f4f8b23c6e1513
tomo18350110 <73381856+tomo18350110@users.noreply.github.com>
2020-12-03 16:32:50 +0900

    Revert "商品出品機能の実装"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,29 +1,9 @@
 class ItemsController < ApplicationController
-  # before_action :move_to_index, except: [:index, :show]
-  before_action :authenticate_user!, only: [:new]
-  # def index
-  #   @items = Item.all
-  # end
-
-  def new
-    @item = Item.new
+  before_action :move_to_index, except: [:index, :show]
+  def index
   end
 
-  def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
+  def move_to_index
+    redirect_to action: :index unless user_sigbed_in?
   end
-
-  private
-  def item_params
-    params.require(:item).permit(:item_name, :image, :explanation, :category_id, :condition_id, :shipping_charges_id, :shipping_area_id, :days_to_ship_id, :item_price).merge(user_id: current_user.id)
-  end
-
-  # def move_to_index
-  #   redirect_to action: :index unless user_signed_in?
-  # end
 end
